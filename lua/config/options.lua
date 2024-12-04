6446733be1980651c52ae00948ef19f54627f595
weikangli <weikangli@deeproute.ai>
2024-12-04 17:16:14 +0800

    update copy file abs path to clippad

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,9 @@
 -- LazyVim auto format
 vim.g.autoformat = false
 vim.opt.relativenumber = false
+vim.keymap.set("n", "<leader>cp", function()
+   local filepath = vim.fn.expand("%:p")
+   vim.fn.setreg("+", filepath) -- 将路径复制到系统剪贴板
+   print("Copied: " .. filepath) -- 提示路径已复制
+end, { noremap = true, silent = true, desc = "Copy absolute file path" })
+
