bdfc789ae86b6b5d478bf2ebe04bc5f8db49d9f4
weikangli <weikangli@deeproute.ai>
2024-12-13 14:12:51 +0800

    add declaration

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -23,3 +23,39 @@ vim.keymap.set('n', '<leader>dm', function()
     print("No mark specified!")
   end
 end, { noremap = true, silent = true, desc = "Delete mark" })
+
+-- Declare
+vim.api.nvim_set_keymap('n', '<leader>dp', ':lua InsertCustomComment("python")<CR>', { noremap = true, silent = true , desc = "Declareation python"})
+vim.api.nvim_set_keymap('n', '<leader>dc', ':lua InsertCustomComment("cpp")<CR>', { noremap = true, silent = true , desc = "Declareation cpp"})
+
+function InsertCustomComment(language)
+    local current_time = os.date("%Y/%m/%d %H:%M:%S")
+    local filename = vim.fn.expand('%:t') -- 获取当前文件名
+    local author = "GauthierLi"
+    local contact = "lwklxh@163.com"
+    local year = 2024
+
+    local comment_start, comment_end = "", ""
+    if language == "python" then
+        comment_start, comment_end = "'''", "'''"
+    elseif language == "cpp" then
+        comment_start, comment_end = "/*", "*/"
+    end
+
+    -- 构造注释模板
+    local comment_template = string.format([[%s
+@File    :   %s
+@Time    :   %s
+@Author  :   %s
+@Version :   1.0
+@Contact :   %s
+@License :   Copyright (C) %s %s, All rights reserved.
+%s
+
+%s
+Description here ...
+%s]], comment_start, filename, current_time, author, contact, year, author, comment_end, comment_start, comment_end)
+    -- 在当前光标位置插入模板
+    vim.api.nvim_put(vim.split(comment_template, "\n"), 'l', true, true)
+end
+
